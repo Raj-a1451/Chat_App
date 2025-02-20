c7b7e5402f9bac613218fcb3aabe92a0f3ef39b3
Raj-a1451 <raj1451a1451@gmail.com>
2025-02-20 23:58:14 +0530

    signup & logout in clinet server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
+import { userAuthStore } from './store/userAuthStore'
+import { Loader } from 'lucide-react'
+import {Toaster} from 'react-hot-toast'
+
 
 const App = () => {
+
+  const { authUser, checkAuth, isCheckingAuth } = userAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+  console.log({ authUser })
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin' />
+      </div>
+    )
+  }
+
   return (
     <div>
-      <Navbar>
-        <Routes>
-          <Route path='/' element={<HomePage/>} />
-          <Route path='/signup' element={<SignupPage/>} />
-          <Route path='/login' element={<LoginPage/>} />
-          <Route path='/settings' element={<SettingsPage/>} />
-          <Route path='/profile' element={<ProfilePage/>} />
-        </Routes>
-      </Navbar>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+        <Route path='/signup' element={!authUser ? <SignupPage />:<Navigate to='/' />} />
+        <Route path='/login' element={!authUser ? <LoginPage />:<Navigate to='/' />} />
+        <Route path='/settings' element={<SettingsPage />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+      </Routes>
+      <Toaster  />
     </div>
   )
 }
